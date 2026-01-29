f4059f031c0e0fc28aa78eeea75f447e92c0a0eb
Andy wu <lpageo@163.com>
2026-01-30 03:38:13 +0800

    fix(ui): define popover theme tokens

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,10 @@ export default {
           DEFAULT: "hsl(var(--accent))",
           foreground: "hsl(var(--accent-foreground))",
         },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
